b268ed9c4b4817f265e52457d730506bfa849b33
friendly-skill-4649 <jeffrey.palmowski@hotmail.com>
2020-07-09 23:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ return "I can't hear you!"
 }
 
 if (string.toUpperCase() === string) {
-  return "YES INDEED"
+  return "YES INDEED!"
 }
 
 if (string === "I love you, Grandma.") {
-  return "I love you too."
+  return "I love you, too."
 }
 }
